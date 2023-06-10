163d69642a409dcd3cbcd13be28de28c821132be
Dominik Kormann <dominikormann@outlook.com>
2023-06-10 16:53:01 +0200

    proto

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
 
 
 
-<div id=face></div>
-<div id=bar></div>
-<h1>Binäre Bauten</h1>
 
-<h2>hallo</h2>
+
+<div id = content>
+
+    <h1>Binäre Bauten</h1>
+    
+    <h2>Beratungshaus und Software-Entwicklung im Herzen Berlins. </h2>
+
+    <div class=text>
+
+        <p>Als Firma im Jahr 2008 ins Geschäftsleben gestellt von einem Team hochqualifizierter Informatiker fokussieren wir uns auf die Felder Softwarearchitektur, Refaktorierung und Projektverantwortung.</p>
+        <p>Trotz komplexer und vielfältig anwendbarer Spezialgebiete sind wir uns mit unseren Kunden einig im Verständnis, dass Klarheit und Schlichtheit erstrebenswerte, aber oft vernachlässigte Qualitätsmerkmale sind.</p>
+        <p>Unsere Erfahrungen haben wir uns in der Vergangenheit individuell und wiederholt auch gemeinsam in unterschiedlichsten Projektsituationen und -positionen im Auftrag namhafter [und genauso auch wenig bekannter, aber deswegen nicht minder einflussreicher, stilbildender oder fordernder] Softwareproduzenten erarbeitet.</p>
+        <p>Neben Kompetenz und Erfahrung sehen wir in Kommunikation, Agilität und interdisziplinärem Interesse die Schlüssel zu erfolgreicher Projektarbeit.</p>
+
+        <div class=text_end></div>
+    </div>  
+
+
+
+</div>
+
+
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+
+
+<div id=content>
+
+<h1>
+    Produkte
+</h1>
+
+<div class= text>
+
+<p>Warum pflegt ein Beratungshaus eine Seite mit dem Titel Produkte? Die Antwort ist ebenso einleuchtend wie simpel:</p>
+
+<p>Software-Werkzeuge sind Teilergebnisse unserer Prozesse und Zeugnisse unserer Kreativität. Sie entstehen in Projekten oder im Rahmen unserer individuellen Weiterentwicklung. Ein guter Berater ist man nicht per se oder automatisch mit Erlangen eines Hochschulabschlusses. Soft-Skills müssen ebenso weiterentwickelt werden wie fachliche Fähigkeiten auf unterschiedlichen Technology-Stacks.</p>
+
+<p>In guten Fällen entwickeln diese Werkzeuge Eigendynamik, wenn sie eine besonders effiziente Unterstützung für Prozesse bieten, die auch außerhalb unseres eigenen Unternehmens existieren, wenn sie besondere Beispiele für unsere Leistungsfähigkeit sind oder wenn sie einfach nur herausragen - unter welchem Aspekt auch immer.</p>
+
+<p>Erreichen sie dann noch einen Reifegrad weit oberhalb von Proof of Concept oder Prototyp, nennen wir sie Produkte - als Oberbegriff für eine breite Spanne von Framework bis App.</p>
+
+<p>Als Produkthaus wollen wir uns noch nicht verstehen. Wohl aber als Dienstleister und Lieferanten, der seinen Kunden über reine Beratungsleistung hinaus Effizienzsteigerung durch den Einsatz valider Tools anbieten kann. In diesem Verständnis werden Sie an dieser Stelle zukünftig eine wachsende Menge Produkte finden ...</p>
+
+<div class=text_end></div>
+
+
+</div>
+
+</div>
+
