c488445c246a5bdfc5842ae48bb3a3cb7fd72ec6
Dominik Kormann <dominikormann@outlook.com>
2023-06-11 18:50:52 +0200

    solution boxes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
+<script>
+    import SolutionItem from "./solutionItem.svelte";
 
-
-
+</script>
 
 
 <div id = content>
@@ -16,11 +17,54 @@
         <p>Unsere Erfahrungen haben wir uns in der Vergangenheit individuell und wiederholt auch gemeinsam in unterschiedlichsten Projektsituationen und -positionen im Auftrag namhafter [und genauso auch wenig bekannter, aber deswegen nicht minder einflussreicher, stilbildender oder fordernder] Softwareproduzenten erarbeitet.</p>
         <p>Neben Kompetenz und Erfahrung sehen wir in Kommunikation, Agilität und interdisziplinärem Interesse die Schlüssel zu erfolgreicher Projektarbeit.</p>
 
-        <div class=text_end></div>
+        <!-- <div class=text_end></div> -->
     </div>  
+
+    <div style="padding-bottom:4em"></div>
+
+    <p class=title>unsere Lösungen:</p>
+
+
+        <div class=solutions>
+
+            <SolutionItem title="Quecksolver" description="Excel arbeit im Team koordinieren."/>
+
+            <SolutionItem title="Development" description="Softwareentwicklung mit Fokus auf Architektur und Refaktorierung."/>
+            <SolutionItem title="Beratung" description="Beratung mit Fokus auf Architektur und Refaktorierung."/>
+            <SolutionItem title="Instandhaltung" description="Instandhaltung mit Fokus auf Architektur und Refaktorierung."/>
+
+        </div>
+        
 
 
 
 </div>
+
+
+<style>
+
+    .title{
+        font-size: 20px;
+        font-weight: bold;
+        /* margin-left:7vh; */
+        margin-top:2em;
+        background-color: var(--blue);
+        color:white;
+        display: initial;
+        padding: .2em 1em;
+    }
+
+    .solutions{
+        display: flex;
+        flex-direction: row;
+        justify-content:left;
+        align-items: left;
+        flex-wrap: wrap;
+        padding-left:7vh;
+        padding-top:2em;
+    }
+
+
+</style>
 
 
---
+++ src/routes/solutionItem.svelte
@@ -0,0 +1,55 @@
+
+
+<script lang='ts'>
+    export var title:string
+    export var description:string
+
+</script>
+
+<div class=solution>
+
+    <a  href="products">
+
+        <div class=display >
+            <h3>{title}</h3>
+            
+            <p>{description}</p>
+        </div>
+    </a>
+
+</div>
+
+
+<style>
+
+    .solution{
+
+        font-size: 20px;
+        width: 18em;
+        height: 12em;
+        /* border: 1px solid black; */
+
+        justify-content: center;
+        align-items: center;
+
+
+        transition: 0.2s ease-in-out;
+        cursor: pointer;
+
+
+    }
+
+    .display{
+        /* border: 1px solid black; */
+        box-shadow: 0px 0px 1em 0px rgba(0, 0, 0, 0.161);
+        width: 14em;
+        height: 8em;
+        margin:auto;
+        padding:1em;
+        transition: 0.2s ease-in-out;
+    }
+    .display:hover{
+        font-size: 22px;
+    }
+
+</style>
